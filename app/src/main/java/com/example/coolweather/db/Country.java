14b1d698e80ef8be5535c47a2e6213c8ce2df43c
xufengfeng <xu-fengfeng@msn.com>
2019-05-15 22:29:52 +0800

    create database and table configs.

--- app/src/main/java/com/example/coolweather/db/Country.java
+++ app/src/main/java/com/example/coolweather/db/Country.java
@@ -3,4 +3,40 @@ package com.example.coolweather.db;
 import org.litepal.crud.DataSupport;
 
 public class Country extends DataSupport {
+    private int id;
+    private String conturyName;
+    private String watherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getConturyName() {
+        return conturyName;
+    }
+
+    public void setConturyName(String conturyName) {
+        this.conturyName = conturyName;
+    }
+
+    public String getWatherId() {
+        return watherId;
+    }
+
+    public void setWatherId(String watherId) {
+        this.watherId = watherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
 }
